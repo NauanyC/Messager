90ccf0c9926a5997249f839295bc73ae38aac81e
Nauany <nauanycc@outlook.com.br>
2021-05-25 12:56:02 -0300

    feat: style navbar

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { HiUserCircle } from "react-icons/hi";
+import { GiStoneBlock } from "react-icons/gi";
+
 import { Container, LeftSideMenu, Navbar } from "./styles";
 
 interface LayoutProps {
@@ -9,7 +12,22 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
   return (
     <Container>
-      <Navbar> Hey I am a navbar</Navbar>
+      <Navbar>
+        <h3>
+          <span>
+            <GiStoneBlock size={24} />
+          </span>
+          FaceBlooks
+        </h3>
+        <div>
+          <p>
+            <span>
+              <HiUserCircle size={24} />
+            </span>
+            Username
+          </p>
+        </div>
+      </Navbar>
       <LeftSideMenu> I am a left side menu! </LeftSideMenu>
       {children}
     </Container>
--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
-export const Navbar = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+export const Navbar = styled.div`
+  flex: 0 0 100%;
+  background: var(--main-blue);
+  height: 65px;
+
+  padding: 20px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  svg {
+    vertical-align: bottom;
+    margin-right: 3px;
+  }
+`;
 export const LeftSideMenu = styled.div``;

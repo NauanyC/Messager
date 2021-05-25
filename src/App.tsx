05c22d2163f11313f3585ee3a638ab2ea08742a5
Nauany <nauanycc@outlook.com.br>
2021-05-25 11:14:19 -0300

    chore: add react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Hey! Talk to me... :(</h1>
+    </div>
+  );
+};
+
+export default App;

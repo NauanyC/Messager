e3f5290298ed7ca383b670adfe8d2b96d104f948
Nauany <nauanycc@outlook.com.br>
2021-05-25 11:34:33 -0300

    chore: add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+import ESLintPlugin from "eslint-webpack-plugin";
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.tsx"),
-  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "index.bundle.js",
+  },
   mode: process.env.NODE_ENV || "development",
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
@@ -17,6 +21,11 @@ module.exports = {
         use: ["babel-loader"],
       },
       {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
         test: /\.(css|scss)$/,
         use: ["style-loader", "css-loader"],
       },
@@ -24,16 +33,14 @@ module.exports = {
         test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
         use: ["file-loader"],
       },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ["ts-loader"],
-      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html"),
+    }),
+    new ESLintPlugin({
+      extensions: ["js", "jsx", "ts", "tsx"],
     }),
   ],
 };

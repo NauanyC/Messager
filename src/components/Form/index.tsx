65659e94e940fa29ff2c9bdad3b27e2f0bd37e75
Nauany <nauanycc@outlook.com.br>
2021-05-25 16:51:39 -0300

    chore: add axios

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -38,28 +38,8 @@ const Chat: React.FC /* <ChatProps> */ = () => {
           </span>
         </Header>
         <Messages>
-          <li className="message-from-own-user">How you doing? </li>
-          <li> Hey!</li>
-          <li>Lorem ipsum dolor sit.</li>
-          <li className="message-from-own-user">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum
-            saepe necessitatibus earum tempora iste illo fugiat asperiores
-            eligendi sed accusamus, omnis possimus eos provident eius
-            praesentium rerum eaque, corrupti nostrum.
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente,
-            ea assumenda soluta harum ut omnis error accusamus rerum. Aut,
-            numquam.
-          </li>
-          <li>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero
-            minus in saepe aut cupiditate reiciendis, facere esse autem at
-            excepturi magnam eaque, repellat alias nemo! Neque voluptates, quasi
-            vel fuga, ipsum vitae minus sit ratione atque excepturi dicta
-            possimus nemo! Cum omnis fugiat pariatur quaerat earum ipsum neque
-            ipsa corrupti.
-          </li>
+          {/*  <li className="message-from-own-user">How you doing? </li>
+          <li> Hey!</li> */}
         </Messages>
         <input type="text" placeholder="Type your message..." />
       </CurrentChat>
